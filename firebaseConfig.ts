5cda59dae4333f1320e3a7c760dfec99950eb4fa
green.carlos <verdeclos@gmail.com>
2026-01-29 14:45:08 -0800

    hidden api keys

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,17 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
+const apiKey = import.meta.env.API_KEY
+const authDomain = process.env.AUTH_DOMAIN
+const projectId = process.env.PROJECT_ID
+const storageBucket = process.env.STORAGE_BUCKET
+const messagingSenderId = process.env.MESSAGE
+const appId = process.env.APP_ID
+
+// todo: add sdks for firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyDHo6bkekUZO2IjFPv-kzDTFWqRs9F0YkA",
-  authDomain: "disco-demo-381b1.firebaseapp.com",
-  projectId: "disco-demo-381b1",
-  storageBucket: "disco-demo-381b1.firebasestorage.app",
-  messagingSenderId: "191295503752",
-  appId: "1:191295503752:web:1df2cdb994ff8297de0e3d",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
 
 // Initialize Firebase
